b3d1cbdc474fe2db460ae45a3190896f5ca51c4e
Sandeep Vittala <applesandy33@gmail.com>
2022-01-05 14:50:05 -0500

    use fs to create a file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-printProfileData(profileDataArgs);
+const [myname, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(myname, github), (err) => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
